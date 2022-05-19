d3532de0a1d9f8b47a0d7980f3612f337c966042
Angga Prihantoro <anggaprihantoro3@gmail.com>
2022-05-19 08:33:23 +0700

    adding needded

---
+++ xml_to_csv.py
@@ -0,0 +1,44 @@
+# Modified From:
+# https://github.comr/datitran/raccoon_dataset/blob/master/xml_to_csv.py
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+root = os.getcwd()
+imagePath = os.path.join(root, 'images')
+trainPath = os.path.join(imagePath, 'train')
+testPath = os.path.join(imagePath, 'test')
+
+os.chdir(root)
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+
+def main():
+    for i in [trainPath, testPath]:
+        image_path = i
+        folder = os.path.basename(os.path.normpath(i))
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv('data/'+folder+'.csv', index=None)
+        print('Successfully converted xml to csv.')
+    
+main()
