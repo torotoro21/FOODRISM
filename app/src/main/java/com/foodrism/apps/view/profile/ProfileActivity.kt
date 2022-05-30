ab0d296d68e613a153bf04df826f44dd6160deec
MuhammadNZaki <zakijack654@gmail.com>
2022-05-30 23:49:32 +0700

    Initial commit

---
+++ app/src/main/java/com/foodrism/apps/view/profile/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.foodrism.apps.view.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.foodrism.apps.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
